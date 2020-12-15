68cb585ef15cd33e2b3958cc30fd7c83c22c8483
alex-wallander <alexwallander@gmail.com>
2020-12-15 15:09:01 -0600

    task 2 complete

--- index.js
+++ index.js
@@ -24,11 +24,13 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+ let finalStage = data.filter(function(stage){
+    return stage["Stage"] === 'Final';
+ });
+ return finalStage;
 }
-
-
+console.log(getFinals(fifaData));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function called getYears to do the following: 
@@ -36,7 +38,7 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
+function getYears(arr,cb) {
     /* code here */
 }
 
